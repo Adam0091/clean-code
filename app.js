324825e44a2623514913cae4f5d2b321b1c13496
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-04-04 18:30:05 +0400

    refactoring: classes and ids redefined by BEM according to rule 3.1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -35,18 +35,19 @@ var createNewTaskElement=function(taskString){
     listItem.className="list-task__item";
 
     label.innerText=taskString;
+    label.className='task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     checkBox.classList.add("task-input");
 
     editInput.type="text";
-    editInput.className="task";
     editInput.classList.add("task-input");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.classList.add("edit");
     editButton.classList.add("button-task");
+    editButton.classList.add("edit");
+
 
     deleteButton.className="delete";
     deleteButton.classList.add("button-task");
@@ -92,7 +93,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -106,7 +107,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
